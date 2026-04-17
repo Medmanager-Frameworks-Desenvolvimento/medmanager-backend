4af5a853cfd8e1dd70d3cae8e555e2b5777cea6e
sarahzxwy <sarahalvesborges@outlook.com>
2026-04-17 14:08:45 -0300

    feat: admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './database/prisma.service';
 import { DatabaseModule } from './database/database.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true,  envFilePath: '.env'}),
+    DatabaseModule, 
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
